b8aa389668c35977e7531d66d60c42e78219a5d6
aLearningLad <mocmanca@gmail.com>
2025-01-05 13:07:27 +0200

    intro page element additions

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 138, 60, 55),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 25,
+            ),
+
+            // shop name
+            Text("Yuki's",
+                style: GoogleFonts.dmSerifDisplay(
+                    fontSize: 28, color: Colors.white)),
+
+            const SizedBox(
+              height: 25,
+            ),
+
+            // icon
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Image.asset('lib/images/sushi.png'),
+            ),
+
+            // title
+            Text(
+              "SUSHI GOODNESS, JUST A CLICK AWAY",
+              style:
+                  GoogleFonts.dmSerifDisplay(fontSize: 40, color: Colors.white),
+            ),
+
+            const SizedBox(
+              height: 10,
+            ),
+
+            // subtitle
+            Text(
+              "The world's best-loved Japanese food parlor at your fingertips",
+              style: TextStyle(color: Colors.grey[300], height: 2),
+            ),
+
+            const SizedBox(
+              height: 25,
+            )
+
+            // get started btn
+          ],
+        ),
+      ),
+    );
   }
 }
