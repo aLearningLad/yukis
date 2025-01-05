623455eabe83f05a380b27d747266e3d09f01dde
aLearningLad <mocmanca@gmail.com>
2025-01-05 23:56:46 +0200

    ui fixes

--- lib/components/food_tile.dart
+++ lib/components/food_tile.dart
@@ -10,52 +10,76 @@ class FoodTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: 160, // Constrain the width of the FoodTile
       decoration: BoxDecoration(
-          color: Colors.grey[100], borderRadius: BorderRadius.circular(20)),
-      margin: EdgeInsets.only(left: 25),
-      padding: const EdgeInsets.all(25),
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(20),
+      ),
+      margin: const EdgeInsets.only(left: 15), // Adjust margin for spacing
+      padding: const EdgeInsets.all(15), // Adjust padding for better fit
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min, // Constrain vertical space
         children: [
-          Image.asset(
-            food.imagePath,
-            height: 140,
-          ),
-
-          // text
-          Text(food.name,
-              style: GoogleFonts.dmSerifDisplay(
-                  fontSize: 20, color: Colors.grey[800])),
-
-          // price + rating
-          SizedBox(
-            width: 160,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-// price
-                Text(
-                  food.price,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.grey[700]),
-                ),
-
-// rating
-                Row(
-                  children: [
-                    Icon(
-                      Icons.star,
-                      color: Colors.yellow[800],
-                    ),
-                    Text(
-                      food.rating,
-                      style: TextStyle(color: Colors.grey[600]),
-                    )
-                  ],
-                )
-              ],
+          // Food image
+          ClipRRect(
+            borderRadius:
+                BorderRadius.circular(15), // Rounded corners for image
+            child: Image.asset(
+              food.imagePath,
+              height: 100, // Reduce height to fit layout better
+              width: double.infinity, // Make the image take full width
+              fit: BoxFit.cover, // Scale image to fit nicely
             ),
-          )
+          ),
+          const SizedBox(height: 10), // Add spacing between elements
+
+          // Food name
+          Text(
+            food.name,
+            style: GoogleFonts.dmSerifDisplay(
+              fontSize: 16, // Slightly smaller font for better layout
+              color: Colors.grey[800],
+            ),
+            overflow: TextOverflow.ellipsis, // Handle long text gracefully
+            maxLines: 1, // Limit text to one line
+          ),
+          const SizedBox(height: 5), // Spacing between text and price
+
+          // Price + Rating
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Price
+              Text(
+                food.price,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[700],
+                  fontSize: 14, // Adjust font size
+                ),
+              ),
+
+              // Rating
+              Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: Colors.yellow[800],
+                    size: 16, // Adjust icon size
+                  ),
+                  const SizedBox(width: 2), // Spacing between icon and text
+                  Text(
+                    food.rating,
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 14, // Adjust font size
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -15,14 +15,11 @@ class MenuPage extends StatefulWidget {
 class _MenuPageState extends State<MenuPage> {
 // food menu
   List<Food> foodMenu = [
-// salmon sushi
     Food(
         name: "Salmon Sushi",
         imagePath: "lib/images/salmon.png",
         price: "R149.99",
         rating: "8.8/10"),
-
-// tuna
     Food(
         name: "Suzuka Tuna",
         imagePath: "lib/images/tuna.png",
@@ -47,136 +44,126 @@ class _MenuPageState extends State<MenuPage> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-// promo banner
-          Container(
-            decoration: BoxDecoration(
-                color: primaryColor, borderRadius: BorderRadius.circular(20)),
-            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
-            margin: const EdgeInsets.symmetric(horizontal: 25),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        //this allows me to scroll
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Promo banner
+              Container(
+                decoration: BoxDecoration(
+                    color: primaryColor,
+                    borderRadius: BorderRadius.circular(20)),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+                margin: const EdgeInsets.symmetric(vertical: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    // promo message
-                    Text(
-                      "Get 25% Promo",
-                      style: GoogleFonts.dmSerifDisplay(
-                          fontSize: 20, color: Colors.white),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // promo message
+                        Text(
+                          "Get 25% Promo",
+                          style: GoogleFonts.dmSerifDisplay(
+                              fontSize: 20, color: Colors.white),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        // redeem btn
+                        MyButton(text: 'Redeem', onTap: () {})
+                      ],
                     ),
-
-                    const SizedBox(
-                      height: 20,
+                    // image
+                    Image.asset(
+                      'lib/images/salmon.png',
+                      height: 100,
                     ),
-
-                    // redeem btn
-                    MyButton(text: 'Redeem', onTap: () {})
                   ],
                 ),
-
-// image
-                Image.asset(
-                  'lib/images/salmon.png',
-                  height: 100,
-                ),
-              ],
-            ),
-          ),
-
-          const SizedBox(
-            height: 25,
-          ),
-
-// search bar
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.circular(20)),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.circular(20)),
               ),
-            ),
-          ),
 
-          const SizedBox(
-            height: 25,
-          ),
+              const SizedBox(
+                height: 25,
+              ),
 
-// menu list
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Text(
-              "Food menu here",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey[700],
-                  fontSize: 18),
-            ),
-          ),
+              // Search bar
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(20)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+                ),
+              ),
 
-          const SizedBox(
-            height: 10,
-          ),
+              const SizedBox(
+                height: 25,
+              ),
 
-          Expanded(
-              child: ListView.builder(
+              // Menu list title
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Text(
+                  "Food menu here",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[700],
+                      fontSize: 18),
+                ),
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              // Menu list
+              SizedBox(
+                height: 220,
+                child: ListView.builder(
                   scrollDirection: Axis.horizontal,
                   itemCount: foodMenu.length,
                   itemBuilder: (context, index) => FoodTile(
-                        food: foodMenu[index],
-                      ))),
-
-          const SizedBox(
-            height: 25,
-          ),
-
-          //  popular food
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.grey[100],
-              borderRadius: BorderRadius.circular(20),
-            ),
-            margin: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
-            padding: const EdgeInsets.all(29),
-            child: Row(
-              children: [
-// image
-                Image.asset(
-                  "lib/images/salmon.png",
-                  height: 60,
+                    food: foodMenu[index],
+                  ),
                 ),
+              ),
 
-// name & price
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              const SizedBox(
+                height: 25,
+              ),
+
+              // Popular food container
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                margin: const EdgeInsets.only(bottom: 25),
+                padding: const EdgeInsets.all(20),
+                child: Row(
                   children: [
-// name
-                    Text(
-                      "Salmon Deluxe",
-                      style: GoogleFonts.dmSerifDisplay(fontSize: 18),
+                    // image
+                    Image.asset(
+                      "lib/images/salmon.png",
+                      height: 60,
                     ),
-
-                    const SizedBox(height: 10),
-
-// price
-                    Text(
-                      "R229.99",
-                      style: TextStyle(color: Colors.grey[700]),
-                    )
+                    // name & price can go here if needed
                   ],
-                )
-              ],
-            ),
-          )
-        ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
