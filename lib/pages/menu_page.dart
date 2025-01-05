7e33d2267b3773cc0703e4009642d0f7117c0a46
aLearningLad <mocmanca@gmail.com>
2025-01-05 15:04:32 +0200

    menu page additions

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:yukis/components/button.dart';
 
 class MenuPage extends StatelessWidget {
   const MenuPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Icon(
+          Icons.menu,
+          color: Colors.grey[900],
+        ),
+        title: Text(
+          "Strand, CPT",
+          style: TextStyle(color: Colors.grey[600]),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+// promo banner
+          Container(
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                    // promo message
+                    Text("Get 25% Promo"),
+
+                    // redeem btn
+                    MyButton(text: 'Redeem', onTap: () {})
+                  ],
+                )
+
+// image
+              ],
+            ),
+          )
+
+// search bar
+
+// menu list
+
+// popular food
+        ],
+      ),
+    );
   }
 }
