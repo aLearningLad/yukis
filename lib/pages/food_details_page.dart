6bb7ad8bcaa5f70be63454bae15497d4697ba269
aLearningLad <mocmanca@gmail.com>
2025-01-06 23:17:52 +0200

    food details additions

--- lib/pages/food_details_page.dart
+++ lib/pages/food_details_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:yukis/models/food.dart';
+import 'package:yukis/themes/colors.dart';
 
 class FoodDetailsPage extends StatefulWidget {
   final Food food;
@@ -12,6 +14,98 @@ class FoodDetailsPage extends StatefulWidget {
 class _FoodDetailsPageState extends State<FoodDetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        foregroundColor: Colors.grey[900],
+      ),
+      body: Column(
+        children: [
+// list view of food details
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: ListView(
+              children: [
+                // image
+                Image.asset(
+                  widget.food.imagePath,
+                  height: 200,
+                ),
+
+                const SizedBox(
+                  width: 25,
+                ),
+
+                // rating
+                Row(
+                  children: [
+                    // star icon
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow[800],
+                    ),
+
+                    const SizedBox(
+                      height: 10,
+                    ),
+
+                    // rating number
+                    Text(
+                      widget.food.rating,
+                      style: TextStyle(
+                          color: Colors.grey[600], fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+
+                // food name
+                Text(
+                  widget.food.name,
+                  style: GoogleFonts.dmSerifDisplay(fontSize: 28),
+                ),
+
+                const SizedBox(
+                  height: 25,
+                ),
+
+                // description
+                Text(
+                  "Description",
+                  style: TextStyle(
+                      color: Colors.grey[900],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+
+                const SizedBox(height: 10),
+
+                Text(
+                  "Expertly sliced salmon with creme almond du plantue, on a bed of sweet potato monash and asparagus",
+                  style: TextStyle(
+                      color: Colors.grey[600], fontSize: 14, height: 2),
+                )
+              ],
+            ),
+          )),
+// price + quantity + add to cart button
+          Container(
+            color: primaryColor,
+            child: Column(
+              children: [
+//  price + quantity
+
+// add to cart button
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
